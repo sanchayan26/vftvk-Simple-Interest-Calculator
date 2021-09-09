aebef14f18be51def3e38c0630aba97d0a7b7f59
sanchayan26 <95sanchayan@gmail.com>
2021-09-09 23:37:43 +0530

    second

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ function compute()
     var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
+    if(principal <=0){
+        alert(" enter positive number");
+        document.getElementById("principal").focus();
 
-    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark> "+rate+" </mark>%\<br\>You will receive an amount of <mark>"+ interest+"</mark>,\<br\>in the year mark><mark "+year+"</mark>\<br\>"
+    }else{
+
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark> "+rate+" </mark>%\<br\>You will receive an amount of <mark>"+ interest+"</mark>,\<br\>in the year mark><mark "+year+"</mark>\<br\>"
+
+    }
+
     
 
     
@@ -17,4 +25,7 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+  
+
 }
+
